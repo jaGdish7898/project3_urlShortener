fbd68f629bba6fdde266e12deaacbfcee4225805
jaGdish7898 <93571870+jaGdish7898@users.noreply.github.com>
2021-12-13 20:20:12 +0530

    update

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,8 +1,33 @@
 
-const AuthorModel = require("../models/blogModel")
+
 var validUrl = require('valid-url');
 const urlModel = require("../models/urlModel");
 const { exists } = require("../models/urlModel");
+
+//------------------------------------- Redis Connection--------------------------------------------------------
+
+const redis = require("redis");
+
+const { promisify } = require("util");
+
+//Connect to redis
+const redisClient = redis.createClient(
+    16851,
+  "redis-16851.c12.us-east-1-4.ec2.cloud.redislabs.com",
+  { no_ready_check: true }
+);
+redisClient.auth("syIZhPb6h5RKOhrvAQgoGJrzrNTWjeAs", function (err) {
+  if (err) throw err;
+});
+
+redisClient.on("connect", async function () {
+  console.log("Connected to Redis..");
+});
+
+//Connection setup for redis
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 
 
 //---------------------------Validation functions-----------------------------------------------------------
@@ -82,15 +107,29 @@ const getUrl = async function(req,res){
 try{
    if(!isValid(req.params.urlCode)){
    return res.status(400).send({status:false,messege:"urlCode is not valid"})
-   }
-   let urlData = await urlModel.findOne({urlCode:req.params.urlCode})
-    if(!isValid(urlData.longUrl)){
-       return res.status(400).send({status:false,messege:"longUrl in db for given urlCode is not valid"})
-    }
-    res.status(200).redirect(urlData.longUrl)
 
+   }
+
+   let UrlFromCache = await GET_ASYNC(`${req.params.urlCode}`)
+
+   if(UrlFromCache){
+       let data=JSON.parse(UrlFromCache)
+       res.status(200).redirect(data.longUrl)
+        // res.send({url:data.longUrl})
+       
+   }else{
+       
+    let urlData = await urlModel.findOne({urlCode:req.params.urlCode})
+    if(urlData){
+        await SET_ASYNC(`${req.params.urlCode}`, JSON.stringify(urlData))
+        res.status(200).redirect(urlData.longUrl)
+    }else{
+        res.status(400).send({status:false,msg:"longUrl is not found for given urlCode"})
+    }
+}
 }
 catch(error){
+    console.log(error)
     return res.status(500).send({status: false,msg: error.message})
 }    
 }
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,34 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-// const AuthorModel=require("../models/authorsModel")
-// const blogModel=require("../models/blogModel")
 
-// const AuthorController= require("../controllers/authorcontroller")
-// const BlogController= require("../controllers/blogController")
-// const commonMw=require("../middleware/commonmiddleware")
-
-
-// router.post('/authors',AuthorController.authorsCollection);
-// router.post('/blogs',commonMw.validator, BlogController.createBlog);
-// router.get("/blogs",commonMw.validator,BlogController.getThisBlog) 
-// router.put('/blogs/:blogId',commonMw.validator,BlogController.updateBlog)
-// router.delete("/blog/:blogId",commonMw.validator,BlogController.deleteBlog)
-// router.delete("/blog",commonMw.validator,BlogController.specificDelete)
-
-////////////////
-
-
-
-// router.post("/login", AuthorController.login)   
 let urlController=require("../controllers/urlController")
 
-
-
-
-
-
+//Apis
+//1)to create short url
 router.post("/url/shorten",urlController.shortenUrl)
+//2) to redirect to original url
 router.get("/:urlCode",urlController.getUrl)
 
 
